444b3f9ae66bd2bf24dbcb7575f81cb57bf06d95
Abhishek Saxena2 <abhisheksaxena2@M-C56R6F7MCW.local>
2024-04-02 03:18:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet,HomeComponent,RouterModule],
   // templateUrl: './app.component.html',
   template:`<main>
   <header class='brand-name'>
     <img class='brand-logo' src="/assets/logo.svg" alt="logo" aria-hidden="true"/>
   </header>
   <section>
-    <app-home></app-home>
+    <router-outlet>
+    <!-- <app-home></app-home> -->
+    </router-outlet>
   </section>
   </main>`,
   styleUrl: './app.component.css',
